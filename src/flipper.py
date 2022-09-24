1432d74ae151039b24c95ef6dac3693f7187b252
Ragz <raghavsarin12@gmail.com>
2022-09-25 00:55:01 +0530

    added flipping simulator + results and percentage calculations

--- src/flipper.py
+++ src/flipper.py
@@ -6,15 +6,22 @@ flipped = 0
 a = 0
 
 print('Coin Flipping Simulator')
-print('Type the number of times you would like to flip a coin.')
+print('Enter the number of times you would like to flip a coin.')
 flips = float(input())
+print('')
 
 while a < flips:
     flip = random.randrange(2)
-    flipped = flipped+1
-    a = a+1
+    flipped = flipped + 1
+    a = a + 1
     if flip == 0:
         heads = heads + 1
     else:
         tails = tails + 1
     print(flip, end=" ")
+
+print(' ')
+print('Results:')
+print(f'Coins Flipped: {flipped}')
+print(f'Percentage of Heads: {100/flips*heads}%')
+print(f'Percentage of Tails: {100/flips*tails}%')
